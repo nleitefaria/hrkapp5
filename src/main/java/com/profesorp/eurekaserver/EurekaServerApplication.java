8031420c9451facc6fd79b4ca1be3ed00615ea74
nleitefaria <nleitefaria@gmail.com>
2020-01-30 11:18:57 +0000

    Update EurekaServerApplication.java

--- src/main/java/com/profesorp/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/profesorp/eurekaserver/EurekaServerApplication.java
@@ -10,7 +10,6 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		System.out.println("@init");
-		System.out.println("*****************************");
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
